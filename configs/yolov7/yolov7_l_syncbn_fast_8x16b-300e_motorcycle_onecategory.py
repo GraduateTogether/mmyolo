623dbc4f28dd241da4be6fe2e10214bbae93bbe5
Jimmy <jimmy3421@gmail.com>
2024-02-18 20:46:08 +0800

    根據參考論文調整超參數
    https://ieeexplore.ieee.org/abstract/document/10185029

--- configs/yolov7/yolov7_l_syncbn_fast_8x16b-300e_motorcycle_onecategory.py
+++ configs/yolov7/yolov7_l_syncbn_fast_8x16b-300e_motorcycle_onecategory.py
@@ -12,8 +12,10 @@ anchors = [
     [(100, 115), (113, 154), (154, 143)]
 ]
 
-max_epochs = 40
-train_batch_size_per_gpu = 12
+base_lr = 0.001
+max_epochs = 50
+
+train_batch_size_per_gpu = 32
 train_num_workers = 4
 
 load_from = 'https://download.openmmlab.com/mmyolo/v0/yolov7/yolov7_l_syncbn_fast_8x16b-300e_coco/yolov7_l_syncbn_fast_8x16b-300e_coco_20221123_023601-8113c0eb.pth'  # noqa
@@ -47,6 +49,7 @@ test_dataloader = dict(
         ann_file='annotations/val.json',
         data_prefix=dict(img='images/')))
 
+_base_.optim_wrapper.optimizer.lr = base_lr
 _base_.optim_wrapper.optimizer.batch_size_per_gpu = train_batch_size_per_gpu
 
 val_evaluator = dict(ann_file=data_root + 'annotations/val.json')
@@ -56,7 +59,7 @@ default_hooks = dict(
     checkpoint=dict(interval=10, max_keep_ckpts=2, save_best='auto'),
     # The warmup_mim_iter parameter is critical.
     # The default value is 1000 which is not suitable for cat datasets.
-    param_scheduler=dict(max_epochs=max_epochs, warmup_mim_iter=10),
+    param_scheduler=dict(max_epochs=max_epochs, warmup_mim_iter=1000),
     logger=dict(type='LoggerHook', interval=5))
 train_cfg = dict(max_epochs=max_epochs, val_interval=10)
 visualizer = dict(vis_backends = [dict(type='LocalVisBackend'), dict(type='WandbVisBackend',init_kwargs={"project": "motorcycle_onecategory"})]) # noqa
